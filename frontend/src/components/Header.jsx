d1be7e5b83b49e3f2e8408a3e9114ad74936a3b3
einsteinkavi98@gmail.com <kavirasan1998@gmail.com>
2023-11-25 22:08:14 +0530

    mern-authht

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div className=' bg-gray-200 shadow-lg shadow-slate-500 h-10 p-2 flex flex-wrap justify-between align-center'>
+        <Link to='/'><h1 className='font-bold'>Authenticate</h1> </Link> 
+        <ul className='flex gap-4 flex-wrap '>
+           <Link to='/profile'><li>Home</li></Link> 
+           <Link to='/about'><li>About</li></Link> 
+           <Link to='/sign-in'><li>Sign in</li></Link>
+            
+        </ul>
+    </div>
+
+  )
+}
+
+export default Header
