c74a707f593250702b9788cec666452737213fb6
einsteinkavi98@gmail.com <kavirasan1998@gmail.com>
2023-11-25 22:37:21 +0530

    create test api

---
+++ backend/controller/user.controller.js
@@ -0,0 +1,6 @@
+const test = (req,res) => {
+  res.json ({
+    message:'otha vera level'
+  });
+}
+export default test
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import PORT from './config.js';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import userdata from './routes/user.routes.js'
 dotenv.config();
 
 const app = express();
@@ -10,6 +11,7 @@ app.listen (PORT , () => {
     console.log(`Server is listening at ${PORT}`);
 });
 
+app.use('/api',userdata)
 mongoose
 .connect(process.env.MONGO)
 .then(() => {
